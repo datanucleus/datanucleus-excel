ed5b37d6a6e5841bcbf7b8285567f7cfb3b0d65c
andyjefferson <andy@datanucleus.org>
2014-02-17 13:44:19 +0000

    Use PropertyNames where possible

--- src/java/org/datanucleus/store/excel/ConnectionFactoryImpl.java
+++ src/java/org/datanucleus/store/excel/ConnectionFactoryImpl.java
@@ -20,6 +20,7 @@ package org.datanucleus.store.excel;
 import java.util.Map;
 
 import org.datanucleus.ExecutionContext;
+import org.datanucleus.PropertyNames;
 import org.datanucleus.exceptions.NucleusException;
 import org.datanucleus.store.StoreManager;
 import org.datanucleus.store.connection.AbstractConnectionFactory;
@@ -45,7 +46,7 @@ public class ConnectionFactoryImpl extends AbstractConnectionFactory
         String url = storeMgr.getConnectionURL();
         if (url == null)
         {
-            throw new NucleusException("You haven't specified persistence property 'datanucleus.ConnectionURL' (or alias)");
+            throw new NucleusException("You haven't specified persistence property '" + PropertyNames.PROPERTY_CONNECTION_URL + "' (or alias)");
         }
 
         int filenameStart = 6;
