870f17e092302735aea8b9e5636cdf73e8884600
andyjefferson <andy@datanucleus.org>
2015-02-27 09:02:39 +0000

    Fix to map handling of key/val not being set

--- src/main/java/org/datanucleus/store/excel/fieldmanager/FetchFieldManager.java
+++ src/main/java/org/datanucleus/store/excel/fieldmanager/FetchFieldManager.java
@@ -483,6 +483,8 @@ public class FetchFieldManager extends AbstractFetchFieldManager
                             String keyStr = keyCmpt.substring(1, keyCmpt.length()-1);
                             String valStr = valCmpt.substring(1, valCmpt.length()-1);
 
+                            boolean keySet = true;
+                            boolean valSet = true;
                             Object key = null;
                             if (keyCmd != null)
                             {
@@ -504,6 +506,7 @@ public class FetchFieldManager extends AbstractFetchFieldManager
                                 {
                                     // Object no longer exists. Deleted by user? so ignore
                                     changeDetected = true;
+                                    keySet = false;
                                 }
                             }
                             else
@@ -546,6 +549,7 @@ public class FetchFieldManager extends AbstractFetchFieldManager
                                 {
                                     // Object no longer exists. Deleted by user? so ignore
                                     changeDetected = true;
+                                    valSet = false;
                                 }
                             }
                             else
@@ -567,7 +571,7 @@ public class FetchFieldManager extends AbstractFetchFieldManager
                                 }
                             }
 
-                            if (!changeDetected)
+                            if (keySet && valSet)
                             {
                                 map.put(key, val);
                             }
