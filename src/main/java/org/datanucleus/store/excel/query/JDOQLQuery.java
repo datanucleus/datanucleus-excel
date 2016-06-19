2d9b1df3bdadcfc33ec073580ed1bd6e676793ae
andyjefferson <andy@datanucleus.org>
2016-06-19 12:12:52 +0100

    Support core issue 100

--- src/main/java/org/datanucleus/store/excel/query/JDOQLQuery.java
+++ src/main/java/org/datanucleus/store/excel/query/JDOQLQuery.java
@@ -25,8 +25,8 @@ import java.util.Map;
 
 import org.datanucleus.ExecutionContext;
 import org.datanucleus.exceptions.NucleusException;
-import org.datanucleus.query.evaluator.JDOQLEvaluator;
-import org.datanucleus.query.evaluator.JavaQueryEvaluator;
+import org.datanucleus.query.inmemory.JDOQLInMemoryEvaluator;
+import org.datanucleus.query.inmemory.JavaQueryInMemoryEvaluator;
 import org.datanucleus.store.StoreManager;
 import org.datanucleus.store.connection.ManagedConnection;
 import org.datanucleus.store.query.AbstractJDOQLQuery;
@@ -103,7 +103,7 @@ public class JDOQLQuery extends AbstractJDOQLQuery
             }
 
             // Evaluate result/filter/grouping/having/ordering in-memory
-            JavaQueryEvaluator resultMapper = new JDOQLEvaluator(this, candidates, compilation,
+            JavaQueryInMemoryEvaluator resultMapper = new JDOQLInMemoryEvaluator(this, candidates, compilation,
                 parameters, ec.getClassLoaderResolver());
             Collection results = resultMapper.execute(true, true, true, true, true);
 
