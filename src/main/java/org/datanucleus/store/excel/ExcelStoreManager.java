37448ea157d1486416c26606674f1cb2b6e63169
andyjefferson <andy@datanucleus.org>
2016-09-13 15:20:24 +0100

    Support core issue 128. Fixes #13

--- src/main/java/org/datanucleus/store/excel/ExcelStoreManager.java
+++ src/main/java/org/datanucleus/store/excel/ExcelStoreManager.java
@@ -138,14 +138,14 @@ public abstract class ExcelStoreManager extends AbstractStoreManager implements 
         schemaHandler.createSchemaForClasses(clsNameSet, null, wb);
     }
 
-    public void createSchema(String schemaName, Properties props)
+    public void createDatabase(String catalogName, String schemaName, Properties props)
     {
-        schemaHandler.createSchema(schemaName, props, null);
+        schemaHandler.createDatabase(catalogName, schemaName, props, null);
     }
 
-    public void deleteSchema(String schemaName, Properties props)
+    public void deleteDatabase(String catalogName, String schemaName, Properties props)
     {
-        schemaHandler.deleteSchema(schemaName, props, null);
+        schemaHandler.deleteDatabase(catalogName, schemaName, props, null);
     }
 
     public void createSchemaForClasses(Set<String> classNames, Properties props)
