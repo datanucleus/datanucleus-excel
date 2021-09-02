fbd69db532c5c9c9624c309b13d8f6b2d74f1e40
andyjefferson <andy@datanucleus.org>
2021-09-02 14:41:18 +0100

    Support core-414

--- src/main/java/org/datanucleus/store/excel/fieldmanager/StoreFieldManager.java
+++ src/main/java/org/datanucleus/store/excel/fieldmanager/StoreFieldManager.java
@@ -235,7 +235,7 @@ public class StoreFieldManager extends AbstractStoreFieldManager
             // Embedded field
             if (RelationType.isRelationSingleValued(relationType))
             {
-                if ((insert && !mmd.isCascadePersist()) || (!insert && !mmd.isCascadeUpdate()))
+                if (!mmd.isCascadePersist())
                 {
                     if (!ec.getApiAdapter().isDetached(value) && !ec.getApiAdapter().isPersistent(value))
                     {
@@ -432,7 +432,7 @@ public class StoreFieldManager extends AbstractStoreFieldManager
         }
         else if (RelationType.isRelationSingleValued(relationType))
         {
-            if ((insert && !mmd.isCascadePersist()) || (!insert && !mmd.isCascadeUpdate()))
+            if (!mmd.isCascadePersist())
             {
                 if (!ec.getApiAdapter().isDetached(value) && !ec.getApiAdapter().isPersistent(value))
                 {
@@ -472,7 +472,7 @@ public class StoreFieldManager extends AbstractStoreFieldManager
             if (mmd.hasCollection())
             {
                 Collection coll = (Collection)value;
-                if ((insert && !mmd.isCascadePersist()) || (!insert && !mmd.isCascadeUpdate()))
+                if (!mmd.isCascadePersist())
                 {
                     // Field doesnt support cascade-persist so no reachability
                     if (NucleusLogger.PERSISTENCE.isDebugEnabled())
